f7fd8b40038e59a88aea2da843989e066fa49c43
Jarosław Gołuchowski <jaroslaw.goluchowski@gmail.com>
2019-05-11 10:52:59 +0200

    Initial spring boot

---
+++ src/main/java/com/goluchowski/jaroslaw/pracainzynierskabackend/PracaInzynierskaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.goluchowski.jaroslaw.pracainzynierskabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaInzynierskaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracaInzynierskaBackendApplication.class, args);
+	}
+
+}
