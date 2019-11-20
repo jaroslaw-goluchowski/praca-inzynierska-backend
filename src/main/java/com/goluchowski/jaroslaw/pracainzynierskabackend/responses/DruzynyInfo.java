a2970536879ffca0ce7e20cac67968a1c83bc633
jaroslawgoluchowski <jaroslaw.goluchowski@gmail.com>
2019-11-20 18:41:13 +0100

    Not sending team picture

--- src/main/java/com/goluchowski/jaroslaw/pracainzynierskabackend/responses/DruzynyInfo.java
+++ src/main/java/com/goluchowski/jaroslaw/pracainzynierskabackend/responses/DruzynyInfo.java
@@ -1,5 +1,6 @@
 package com.goluchowski.jaroslaw.pracainzynierskabackend.responses;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.goluchowski.jaroslaw.pracainzynierskabackend.model.Siatkarze;
 import lombok.Data;
 
@@ -12,6 +13,7 @@ public class DruzynyInfo {
 
     private Long data_zalozenia;
 
+    @JsonIgnore
     private byte[] zdjecie_druzynowe;
 
     private byte[] logo;
